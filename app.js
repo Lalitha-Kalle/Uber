ca1c55515bc2fda5069171c7d4f05f96584de4ce
Lalitha-Kalle <kallelalitha04@gmail.com>
2025-01-18 16:00:55 +0530

    db connection done and model creation done

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const express = require("express")
 const cors = require("cors")
 const app = express()
 
+const connectToDB = require("./db/db")
+connectToDB()
+
+app.use(cors)
+
 app.get("/", (req, res) => {
     res.send("Hello World!")
 })
---
+++ models/user.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose")
+
+const userSchema = mongoose.Schema({
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            minlength: [3, 'First name must be at least 3 characters long']
+        },
+        lastname: {
+            type: String,
+            minlength: [3, 'Last name must be at least 3 characters long']
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [5, "Email must be at least 5 characters long"]
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    sockerId: {
+        type: String,
+    }
+})
+
+
+const userModel = mongoose.model("user", userSchema)
