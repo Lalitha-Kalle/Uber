037af8a3174c43f49263d63c2ce11cadc55d2fee
Lalitha-Kalle <kallelalitha04@gmail.com>
2025-01-18 16:12:24 +0530

    "methods created in model and controller created"

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 const userSchema = mongoose.Schema({
     fullname: {
@@ -27,6 +29,19 @@ const userSchema = mongoose.Schema({
         type: String,
     }
 })
+
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id}, process.env.JWT_SECRET)
+    return token
+} 
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10)
+}
 
 
 const userModel = mongoose.model("user", userSchema)
